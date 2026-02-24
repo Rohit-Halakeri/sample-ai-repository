9f5f5c39247b73840cb34e6d7b8c388662cced2e
tecsxpert <dev@tecsxpert.xyz>
2026-02-24 11:59:13 +0530

    Added Exception handling and Datastrucutres for tuples, dictionaries and setsw

--- data-structures.py
+++ data-structures.py
@@ -41,9 +41,50 @@ mixed=[1,"Dennis",25.4,True]
 # for i in range(1,6):
 #     squares.append(i ** 2)
 
-squares = [i ** 2 for i in range(1,6)]   
-print(squares)
+# squares = [i ** 2 for i in range(1,6)]   
+# print(squares)
 
 #Tuples
+coordinates=(10,20)
+person = ("kavya",25,"Chitradurga")
+#print(person[2])
+
+name,age,district=person
+
+#print(f"I am {name}, from {district}. I am {age} years old")
+
 #Dictionaries
+mathClass={}
+
+student={
+    "name":"Dennis",
+    "age":25,
+    "grade":"A",
+    "courses":["Math","Science","Social Science"]
+}
+
+#print(student["name"])
+student["phone"]="8908908900"
+
+#print(student.get("phone","User's Phone number doesn't exist"))
+
+student['age']=26
+#print(student)
+
+#student.pop("grade")
+#print(student)
+# for key in student:
+#     print(f"{key}: {student[key]}")
+
+# for key,value in student.items():
+#     print(f"{key}:{value}")
+
 #Sets
+
+empty_set=set()
+numbers=[1,2,3,1,3,4,3,6,7,888,9,2,4,5,5,5,5,5,5]
+unique_numbers=set(numbers)
+print(numbers)
+print(unique_numbers)
+
+unique_numbers.discard(999)
